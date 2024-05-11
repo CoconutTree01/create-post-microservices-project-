55f8ecdf73c0f930d66ed87bd5084a55b983ec90
OlegsIljanovs <42992921+CoconutTree01@users.noreply.github.com>
2024-05-11 10:58:51 +0300

    added a comment moderation service

--- blog/event-bus/index.js
+++ blog/event-bus/index.js
@@ -10,18 +10,22 @@ app.post('/events', (req, res) => {
     const event = req.body;
 
     axios.post('http://localhost:4000/events', event).catch((err) => {
-        console.log(err.message);
-      });;
+      console.log(err.message);
+    });;
     axios.post('http://localhost:4001/events', event).catch((err) => {
-        console.log(err.message);
-      });;
+      console.log(err.message);
+    });;
     axios.post('http://localhost:4002/events', event).catch((err) => {
-        console.log(err.message);
-      });;
+      console.log(err.message);
+    });;
+    axios.post('http://localhost:4003/events', event).catch((err) => {
+      console.log(err.message);
+    });;
 
-    res.send({ status: 'OK' });
+
+  res.send({ status: 'OK' });
 })
 
 app.listen(4005, () => {
-    console.log('Listening on 4005');
+  console.log('Listening on 4005');
 })
